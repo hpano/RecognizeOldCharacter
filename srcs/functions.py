ff0304f923872218376487b0f56e9f14b30bfec7
hpano <hase.64.34@gmail.com>
2019-07-26 18:02:19 +0900

    first commit

---
+++ srcs/functions.py
@@ -0,0 +1,171 @@
+import os
+import numpy as np
+
+
+def softmax(x):
+    if x.ndim == 2:
+        x = x.T
+        x = x - np.max(x, axis=0)
+        y = np.exp(x) / np.sum(np.exp(x), axis=0)
+        return y.T
+
+    x = x - np.max(x)  # オーバーフロー対策
+    return np.exp(x) / np.sum(np.exp(x))
+
+
+def cross_entropy_error(y, t):
+    delta = 1e-7
+    if y.ndim == 1:
+        t = t.reshape(1, t.size)
+        y = y.reshape(1, y.size)
+
+    if t.size == y.size:
+        t = t.argmax(axis=1)
+
+    batch_size = y.shape[0]
+    return -np.sum(np.log(y[np.arange(batch_size), t] + delta)) / batch_size
+
+
+def numerical_gradient(f, x):
+    h = 1e-4
+    grad = np.zeros_like(x)
+
+    it = np.nditer(x, flags=['multi_index'], op_flags=['readwrite'])
+    while not it.finished:
+        idx = it.multi_index
+        tmp_val = x[idx]
+        x[idx] = float(tmp_val) + h
+        fxh1 = f(x)  # f(x + h)
+
+        x[idx] = tmp_val - h
+        fxh2 = f(x)
+
+        grad[idx] = (fxh1 - fxh2) / (2 * h)
+        x[idx] = tmp_val  # 値を元に戻す
+        it.iternext()
+
+    return grad
+
+
+def im2col(input_data, filter_h, filter_w, stride=1, pad=0):
+    """
+    Parameters
+    ----------
+    input_data : (データ数, チャンネル, 高さ, 幅)の4次元配列からなる入力データ
+    filter_h : フィルターの高さ
+    filter_w : フィルターの幅
+    stride : ストライド
+    pad : パディング
+    Returns
+    -------
+    col : 2次元配列
+    """
+    N, C, H, W = input_data.shape
+    out_h = (H + 2 * pad - filter_h) // stride + 1
+    out_w = (W + 2 * pad - filter_w) // stride + 1
+
+    img = np.pad(input_data, [(0, 0), (0, 0), (pad, pad), (pad, pad)], 'constant')
+    col = np.zeros((N, C, filter_h, filter_w, out_h, out_w))
+
+    for y in range(filter_h):
+        y_max = y + stride * out_h
+        for x in range(filter_w):
+            x_max = x + stride * out_w
+            col[:, :, y, x, :, :] = img[:, :, y:y_max:stride, x:x_max:stride]
+
+    col = col.transpose(0, 4, 5, 1, 2, 3).reshape(N * out_h * out_w, -1)
+    return col
+
+
+def col2im(col, input_shape, filter_h, filter_w, stride=1, pad=0):
+    """
+    Parameters
+    ----------
+    col :
+    input_shape : 入力データの形状（例：(10, 1, 28, 28)）
+    filter_h :
+    filter_w
+    stride
+    pad
+    Returns
+    -------
+    """
+    N, C, H, W = input_shape
+    out_h = (H + 2 * pad - filter_h) // stride + 1
+    out_w = (W + 2 * pad - filter_w) // stride + 1
+    col = col.reshape(N, out_h, out_w, C, filter_h, filter_w).transpose(0, 3, 4, 5, 1, 2)
+
+    img = np.zeros((N, C, H + 2 * pad + stride - 1, W + 2 * pad + stride - 1))
+    for y in range(filter_h):
+        y_max = y + stride * out_h
+        for x in range(filter_w):
+            x_max = x + stride * out_w
+            img[:, :, y:y_max:stride, x:x_max:stride] += col[:, :, y, x, :, :]
+
+    return img[:, :, pad:H + pad, pad:W + pad]
+
+
+def index2code(index):
+    codes = ["U+3042", "U+3044", "U+3046", "U+3048", "U+304A", "U+304B", "U+304D", "U+304F",
+             "U+3051", "U+3053", "U+3055", "U+3057", "U+3059", "U+305B", "U+305D", "U+305F",
+             "U+3061", "U+3064", "U+3066", "U+3068", "U+306A", "U+306B", "U+306C", "U+306D", "U+306E", "U+306F",
+             "U+3072", "U+3075", "U+3078", "U+307B", "U+307E", "U+307F",
+             "U+3080", "U+3081", "U+3082", "U+3084", "U+3086", "U+3088", "U+3089", "U+308A", "U+308B", "U+308C", "U+308D", "U+308F",
+             "U+3090", "U+3091", "U+3092", "U+3093"]
+    return codes[index]
+
+
+def code2index(code):
+    codes = ["U+3042", "U+3044", "U+3046", "U+3048", "U+304A", "U+304B", "U+304D", "U+304F",
+             "U+3051", "U+3053", "U+3055", "U+3057", "U+3059", "U+305B", "U+305D", "U+305F",
+             "U+3061", "U+3064", "U+3066", "U+3068", "U+306A", "U+306B", "U+306C", "U+306D", "U+306E", "U+306F",
+             "U+3072", "U+3075", "U+3078", "U+307B", "U+307E", "U+307F",
+             "U+3080", "U+3081", "U+3082", "U+3084", "U+3086", "U+3088", "U+3089", "U+308A", "U+308B", "U+308C", "U+308D", "U+308F",
+             "U+3090", "U+3091", "U+3092", "U+3093"]
+    return codes.index(code)
+
+
+def divide_img(targets, target_name, num_train, img_size):
+    load_size = min(num_train, len(targets))
+    pro_size = 20
+    memory_size = 0
+
+    if not os.path.exists("./{}_img.npy".format(target_name, load_size)):
+        # print("not found")
+        div_targets = np.empty((0, 1, img_size, img_size))
+        div_codes = np.empty((0, 48))
+        code_reliability = np.zeros((2, 48, 48), int)
+    else:
+        div_targets = np.load("{}_img.npy".format(target_name))
+        div_codes = np.load("{}_code.npy".format(target_name))
+        code_reliability = np.load("{}_code_reliability.npy".format(target_name))
+        # code_reliability = np.zeros((2, 48, 48), int)
+        memory_size = int(div_targets.shape[0] / 3)
+        # print(memory_size)
+
+    for i in range(memory_size, load_size):
+        pro_rate = int((i / load_size) * pro_size)
+        pro_bar = ("=" * (pro_rate)) + (" " * int(pro_size - pro_rate))
+        print("\rloading data ... [{}] {}/{}".format(pro_bar, i, load_size), end="")
+
+        img, codes = targets[i]
+        img = img.convert('L')  # Gray scale
+        img = np.array(img.resize((img_size, img_size * 3)))
+
+        for j in range(3):
+            div_targets = np.append(div_targets, [[img[(j * img_size):((j + 1) * img_size)]]], axis=0)
+            one_hot_code = np.zeros(48, int)
+            one_hot_code[code2index(codes[j])] = 1
+            div_codes = np.append(div_codes, [one_hot_code], axis=0)
+
+        code_reliability[1][code2index(codes[0])][code2index(codes[1])] += 1
+        code_reliability[0][code2index(codes[1])][code2index(codes[0])] += 1
+        code_reliability[1][code2index(codes[1])][code2index(codes[2])] += 1
+        code_reliability[0][code2index(codes[2])][code2index(codes[1])] += 1
+
+    np.save("{}_img".format(target_name), div_targets)
+    np.save("{}_code".format(target_name), div_codes)
+    np.save("{}_code_reliability".format(target_name), code_reliability)
+    print("\nload finished.")
+
+    return div_targets[:load_size * 3], div_codes[:load_size * 3]
---
+++ srcs/myalgorithm.py
@@ -0,0 +1,65 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from srcs.convnet import ConvNet
+from srcs.functions import divide_img
+from srcs.trainer import Trainer
+
+
+class MyAlgorithm():
+    """
+    Build your algorithm.
+    """
+    def build_model(self, traindata, valdata):
+        max_epochs = 30
+        num_train = 12000
+        batch_size = 2000
+        img_size = 32
+
+        # データの読み込み
+        (x_train, t_train) = divide_img(traindata, "traindata", num_train, img_size)
+        (x_val, t_val) = divide_img(valdata, "valdata", num_train, img_size)
+
+        # 処理に時間のかかる場合はデータを削減
+        # x_train, t_train = x_train[:5000], t_train[:5000]
+        # x_val, t_val = x_val[:1000], t_val[:1000]
+
+        print("set model ...")
+        network = ConvNet(input_dim=(1, img_size, img_size),
+                          conv_param={'filter_num': 30, 'filter_size': 5, 'pad': 0, 'stride': 1},
+                          hidden_size=100, output_size=48, weight_init_std=0.01)
+
+        trainer = Trainer(network, x_train, t_train, x_val, t_val,
+                          epochs=max_epochs, mini_batch_size=batch_size,
+                          optimizer='Adam', optimizer_param={'lr': 0.001},
+                          evaluate_sample_num_per_epoch=900)
+        print("train ...")
+        trainer.train()
+
+        # パラメータの保存
+        network.save_params("params.pkl")
+        print("Saved Network Parameters!")
+
+        # グラフの描画
+        markers = {'train': 'o', 'test': 's'}
+        x = np.arange(max_epochs)
+        plt.plot(x, trainer.train_acc_list, marker='o', label='train', markevery=2)
+        plt.plot(x, trainer.test_acc_list, marker='s', label='test', markevery=2)
+        plt.plot(x, trainer.train_3_acc_list, marker='o', label='train 3', markevery=2)
+        plt.plot(x, trainer.test_3_acc_list, marker='s', label='test 3', markevery=2)
+        plt.xlabel("epochs")
+        plt.ylabel("accuracy")
+        plt.ylim(0, 1.0)
+        plt.legend(loc='lower right')
+        plt.show()
+
+    # Output is expected as list, ['U+304A','U+304A','U+304A']
+    def predict(self, img):
+        img = np.array(img.convert('L'))
+        feat = np.resize(img, 768)  # feature vector
+        feat = np.reshape(feat, (3, 256))
+        dist = [0] * 3
+        y_pred = ['U+0000'] * 3
+        for i in range(3):
+            dist[i] = np.linalg.norm(self.model - feat[i], axis=1)  # measure distance
+            y_pred[i] = self.y_train[np.argmin(dist[i])]  # Get the closest
+        return y_pred
