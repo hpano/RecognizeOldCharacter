7b7af4611035ea1a4d8dfde23f9f2562522cbc3d
hpano <hase.64.34@gmail.com>
2019-08-01 00:28:50 +0900

    fix set_data

--- test.py
+++ test.py
@@ -13,40 +13,41 @@ def set_data(targets, target_name, num_train, filter, img_size):
     div_targets = np.empty((0, 1, img_size, img_size))
     div_codes = np.empty((0, 48), dtype=np.uint8)
 
-    # if not os.path.exists("./{}_img.npy".format(target_name, load_size)):
-    #     # print("not found")
-    #     div_targets = np.empty((0, 1, img_size, img_size))
-    #     div_codes = np.empty((0, 48))
-    #     code_reliability = np.zeros((2, 48, 48), int)
-    # else:
-    #     div_targets = np.load("{}_img.npy".format(target_name))
-    #     div_codes = np.load("{}_code.npy".format(target_name))
-    #     code_reliability = np.load("{}_code_reliability.npy".format(target_name))
-    #     # code_reliability = np.zeros((2, 48, 48), int)
-    #     memory_size = int(div_targets.shape[0] / 3)
-    #     # print(memory_size)
+    if not os.path.exists("./{}_{}_img.npy".format(target_name, load_size)):
+        # print("not found")
+        div_targets = np.empty((0, 1, img_size, img_size))
+        div_codes = np.empty((0, 48))
+        code_reliability = np.zeros((2, 48, 48), int)
+    else:
+        div_targets = np.load("{}_{}_img.npy".format(target_name, load_size))
+        div_codes = np.load("{}_{}_code.npy".format(target_name, load_size))
+        code_reliability = np.load("{}_{}_code_reliability.npy".format(target_name, load_size))
+        # code_reliability = np.zeros((2, 48, 48), int)
+        # memory_size = int(div_targets.shape[0] / 3)
+        # print(memory_size)
 
     for i in range(memory_size, load_size):
         pro_rate = int((i / load_size) * pro_size)
         pro_bar = ("=" * (pro_rate)) + (" " * int(pro_size - pro_rate))
-        print("\rloading data ... [{}] {}/{}".format(pro_bar, i, load_size), end="")
+        print("\rloading {} ... [{}] {}/{}".format(target_name, pro_bar, i, load_size), end="")
 
         img, codes = targets[i]
         div_targets = np.append(div_targets, div_img(traindata[i][0], filter, img_size), axis=0)
         one_hot = np.identity(48, dtype=np.uint8)[codes]
         div_codes = np.append(div_codes, one_hot, axis=0)
-        print(codes)  # for debug
-        print(one_hot)  # for debug
 
         # 要修正
-        code_reliability[1][codes[0]][codes[1]] += 1
-        code_reliability[0][codes[1]][codes[0]] += 1
-        code_reliability[1][codes[1]][codes[2]] += 1
-        code_reliability[0][codes[2]][codes[1]] += 1
+        code0 = codes[0]
+        code1 = codes[1]
+        code2 = codes[2]
+        code_reliability[1][code0][code1] += 1
+        code_reliability[0][code1][code0] += 1
+        code_reliability[1][code1][code2] += 1
+        code_reliability[0][code2][code1] += 1
 
-    # np.save("{}_img".format(target_name), div_targets)
-    # np.save("{}_code".format(target_name), div_codes)
-    # np.save("{}_code_reliability".format(target_name), code_reliability)
+    np.save("{}_{}_img".format(target_name, load_size), div_targets)
+    np.save("{}_{}_code".format(target_name, load_size), div_codes)
+    np.save("{}_{}_code_reliability".format(target_name, load_size), code_reliability)
     print("\nload finished.")
 
     return div_targets[:load_size * 3], div_codes[:load_size * 3]
@@ -85,14 +86,16 @@ datapath = './dataset/'
 # Load dataset
 targets = AlconTargets(datapath, train_ratio=0.99)
 traindata = AlconDataset(datapath, targets.train, isTrainVal=True)
-num_train = 2
+num_train = 10000
 img_size = 32
 filter = Image.open("filter2.jpg").convert('L')
 filter = np.array(filter.resize((img_size, img_size)))
-# np.set_printoptions(threshold=np.inf)
+np.set_printoptions(threshold=np.inf)
 
-(x_train, t_train) = set_data(traindata, "train", num_train, filter, img_size)
-print(t_train)  # for debug
-for i in range(num_train * 3):
-    imgg = Image.fromarray(np.uint8(x_train[i][0]))
-    imgg.show()
+(x_train, t_train) = set_data(traindata, "test", num_train, filter, img_size)
+# print(t_train)  # for debug
+# for i in range(num_train * 3):
+#     imgg = Image.fromarray(np.uint8(x_train[i][0]))
+#     imgg.show()
+code_reliability = np.load("{}_{}_code_reliability.npy".format("test", num_train))
+print(code_reliability)
